3e31c67d1ec4d9de3cccf2f9be820f9e0da0c2ab
Maxence1502 <email-me@maxence-salomon.fr>
2023-03-01 23:44:01 +0100

    List slack users

--- unifai.js
+++ unifai.js
@@ -4,25 +4,36 @@ const { SlackAdapter, SlackEventMiddleware } = require('botbuilder-adapter-slack
 const dotenv = require('dotenv');
 dotenv.config();
 
-async function unifaiInit () {
-    const adapter = new SlackAdapter({
-        clientSigningSecret: process.env.SIGNING_SECRET,
-        botToken:  process.env.BOT_TOKEN
-    })
+const adapter = new SlackAdapter({
+    clientSigningSecret: process.env.SIGNING_SECRET,
+    botToken:  process.env.BOT_TOKEN
+})
 
-    adapter.use(new SlackEventMiddleware())
+adapter.use(new SlackEventMiddleware())
 
-    const controller = new Botkit({
-        webhook_uri: '/api/messages',
-        adapter: adapter
-    })
+const controller = new Botkit({
+    webhook_uri: '/api/messages',
+    adapter: adapter
+})
 
-    controller.ready(() => {
-        controller.hears(['hello', 'hi'], ['message', 'direct_message'],
-            async (bot, message) => {
-                await bot.reply(message, 'Meow. :smile_cat:')
-            })
-    })
-}
+controller.ready(() => {
+    controller.hears(['test'], ['direct_message'],
+        async (bot, message) => {
+            await bot.reply(message, message + ':smile_cat:')
+        })
+})
 
-unifaiInit()
+console.log("Debug list users")
+
+controller.on('bot_ready', (bot) => {
+    console.log(`Le bot ${bot.identity.name} est en ligne !`);
+
+    bot.api.users.list({}, (err, res) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        const users = res.members.filter((user) => !user.is_bot && !user.deleted);
+        users.forEach((user) => console.log(user.name, user.id));
+    });
+});
